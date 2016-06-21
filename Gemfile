560f01c34348efdbd1206be809e9a6b31961279e
bowei <bowei.han100@gmail.com>
2016-06-21 14:23:17 -0400

    final version of rainforest

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'pg'
 
+gem 'sqlite3'
 gem 'bcrypt', '~> 3.1.10'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
 
   before_action :ensure_logged_in, only: [:create, :destroy]
+  # here you are LOADING the post because you are rendering the review on the product page
   before_action :load_product
 
   def show
@@ -9,7 +10,13 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @product.reviews.build(review_params)
+    #OR @review = @product.build_review(review_params)
     @review.user = current_user
+
+    #alternatively
+    #@review = Review.new(review_params)
+    #@review.product = @product
+    #@review.user = current_user
 
     # Check out this article on [.build](http://stackoverflow.com/questions/783584/ruby-on-rails-how-do-i-use-the-active-record-build-method-in-a-belongs-to-rel)
     # You could use a longer alternate syntax if it makes more sense to you
@@ -21,6 +28,7 @@ class ReviewsController < ApplicationController
     # )
 
     if @review.save
+      #or @products instead of products_url
       redirect_to products_url, notice: 'Review created successfully'
     else
       render 'products/show'
